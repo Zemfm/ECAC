2ef7d3f05da6ffae1e48b1bf5d0722e2446a3395
José Mendes <zemfmendes@gmail.com>
2019-10-01 00:10:50 +0100

    added py script to calc min balance

---
+++ python/min_balance.py
@@ -0,0 +1,48 @@
+# library
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+import pandas as pd
+
+import numpy as np
+
+
+import timeit 
+
+
+def main():
+
+
+
+    trans = pd.read_csv("files/trans_train_py.csv", sep=';', usecols = ['account_id', 'balance'])
+
+    loans = pd.read_csv("files/loan_train.csv", sep=';', usecols = ['account_id', 'status'])
+
+
+    min_bal = pd.read_csv("min_balance.csv", sep=',')
+
+    trans = trans.sort_values('balance')
+
+    trans = trans.drop_duplicates(subset = 'account_id', keep = "first")
+  
+
+    final_frame = pd.merge(loans, trans, how ='inner', on ='account_id').sort_values('balance')
+
+    final_frame.to_csv(r'min_balance.csv', index =False)
+
+
+
+
+    colors = (0,0,1)
+    area = np.pi*5
+
+    plt.scatter(final_frame["balance"], final_frame["status"], s=area, c=colors, alpha=0.5)
+    plt.title('Minimum balance')
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.show()
+
+
+
+if __name__ == '__main__':
+    main()
