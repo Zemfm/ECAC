5fb0b6e1e7c2adc88a0609f95b69ee238bddf288
José Mendes <zemfmendes@gmail.com>
2019-10-03 21:44:13 +0100

    added features and random forest model

--- python/min_balance.py
+++ python/min_balance.py
@@ -14,7 +14,11 @@ def main():
 
 
     #versao diferente do csv, sem aspas no nome das colunas
-    trans = pd.read_csv("files/trans_train_py.csv", sep=';', usecols = ['account_id', 'balance'])
+
+    pd.read_csv("files/trans_train_py.csv").to_csv("files/trans_train_py_2.csv", sep=';', index=False)
+
+
+    trans = pd.read_csv("files/trans_train_py_2.csv", sep=';', usecols = ['account_id', 'balance'])
 
     loans = pd.read_csv("files/loan_train.csv", sep=';', usecols = ['account_id', 'status'])
 
@@ -24,22 +28,37 @@ def main():
     trans = trans.drop_duplicates(subset = 'account_id', keep = "first")
   
 
-    final_frame = pd.merge(loans, trans, how ='inner', on ='account_id').sort_values('balance')
+    final_frame = pd.merge(loans, trans, how ='left', on ='account_id').sort_values('balance')
 
-    final_frame.to_csv(r'min_balance.csv', index =False)
-
-
+    final_frame.to_csv(r'min_balance_train.csv', index =False)
 
 
-    colors = (0,0,1)
-    area = np.pi*5
 
-    plt.scatter(final_frame["balance"], final_frame["status"], s=area, c=colors, alpha=0.5)
-    plt.title('Minimum balance')
-    plt.xlabel('x')
-    plt.ylabel('y')
-    plt.yticks(np.arange(-1, 1, 2.0))
-    plt.show()
+
+    trans_test = pd.read_csv("files/trans_test.csv", sep=';', usecols = ['account_id', 'balance'])
+
+    loans_test = pd.read_csv("files/loan_test.csv", sep=';', usecols = ['account_id', 'loan_id'])
+
+    trans_test = trans_test.sort_values('balance')
+
+    trans_test = trans_test.drop_duplicates(subset = 'account_id', keep = "first")
+
+    test_frame = pd.merge(loans_test, trans_test, how ='left', on ='account_id').sort_values('balance')
+
+    test_frame.to_csv(r'min_balance_test.csv', index =False)
+
+
+
+
+    #colors = (0,0,1)
+    #area = np.pi*5
+
+   # plt.scatter(final_frame["balance"], final_frame["status"], s=area, c=colors, alpha=0.5)
+    #plt.title('Minimum balance')
+    #plt.xlabel('x')
+    #plt.ylabel('y')
+    #plt.yticks(np.arange(-1, 1, 2.0))
+    #plt.show()
 
 
 
